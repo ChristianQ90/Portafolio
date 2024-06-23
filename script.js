91f599edf4e0a835adf55295acb4b0956bc7d95f
ChristianQ90 <christianq90@gmail.com>
2024-06-23 11:54:45 +0200

    Primer commit para subir Portafolio

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(this);
+
+    fetch(this.action, {
+        method: this.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            document.getElementById('message').classList.add('show');
+            setTimeout(() => {
+                document.getElementById('message').classList.remove('show');
+            }, 3000);
+            this.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Error al enviar el mensaje");
+                }
+            });
+        }
+    }).catch(error => console.error('Error:', error));
+});
